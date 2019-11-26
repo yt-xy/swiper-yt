06c2e8f1f2e185cf102e93715ee4971cc489e2da
yt-xy <1771041565@qq.com>
2019-11-26 23:01:11 +0800

    添加了api接口

--- swiper/urls.py
+++ swiper/urls.py
@@ -15,5 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 
+from user import apis as user_api
+
 urlpatterns = [
+    url(r'^api/user/get_vcode', user_api.get_vcode),
+    url(r'^api/user/submit_vcode', user_api.submit_vcode),
+    url(r'^api/user/get_profile', user_api.get_profile),
+    url(r'^api/user/set_profile', user_api.set_profile),
+    url(r'^api/user/upload_avatar', user_api.upload_avatar),
 ]
--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 获取短信验证码
+def get_vcode(request):
+    return
+
+# 通过验证码登录、注册
+def submit_vcode(request):
+    return
+
+# 获取个人资料
+def get_profile(request):
+    return
+
+# 修改个人资料
+def set_profile(request):
+    return
+
+# 头像上传
+def upload_avatar(request):
+    return
